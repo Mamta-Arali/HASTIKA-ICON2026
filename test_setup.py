33d2ec4ca5e32ebd48d07f01afe43fc990de7a66
Mamta-Arali <aralimamta8@gmail.com>
2026-09-15 19:06:50 +0530

    Initial project setup

---
+++ test_setup.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import sklearn
+import matplotlib
+import seaborn
+
+print("All required libraries are working!")
+print("Pandas version:", pd.__version__)
+print("NumPy version:", np.__version__)
+print("Scikit-learn version:", sklearn.__version__)
